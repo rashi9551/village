a99883733bcd75b95a7546f53105421d1241fec2
rashi9551 <rashidroz1361@gmail.com>
2023-11-22 12:54:21 +0530

    otp

---
+++ server/routers/user.js
@@ -0,0 +1,26 @@
+const express =require("express")
+const usrouter =express.Router()
+const usercontroller = require("./../controller/userController")
+
+
+
+
+usrouter.get("/",usercontroller.index)
+usrouter.get("/signin",usercontroller.signin)
+usrouter.get("/signup",usercontroller.signup)
+usrouter.post("/signotp",usercontroller.signotp)
+usrouter.get("/otp",usercontroller.otp)
+usrouter.post("/verifyotp",usercontroller.verifyotp)
+usrouter.post("/resendotp",usercontroller.resendotp)
+usrouter.post("/login",usercontroller.login)
+usrouter.get("/forgotpassword",usercontroller.forgotpassword)
+usrouter.post("/forgotverify",usercontroller.forgotverify)
+usrouter.get("/newpassword",usercontroller.newpassword)
+
+
+
+
+
+
+module.exports=usrouter
+
